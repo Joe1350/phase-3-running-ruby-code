44c4af496d915734074779712c32fcc5a5b9bdfd
Joseph Martin <josephmartin1350@gmail.com>
2022-02-14 19:59:09 -0700

    commit tree b9cf9b623de569f1119beaf5e1346fb2936c0d0f

---
+++ app.rb
@@ -0,0 +1,26 @@
+# app.rb
+
+# is rubys comment indicator(// is javascripts)
+#puts is console.log()
+
+puts "Hello World!" # preferred by most
+# puts("Hello world!") # is another option
+
+# puts "Hello world!"
+# puts "Hello world!"
+# puts "Hello world!"
+
+# print "Hello world!"
+# print "Hello world!"
+# print "Hello world!"
+
+# puts [1, 2, 3]
+
+# This is the equivalent of calling:
+# puts [1, 2, 3].inspect
+p [1, 2, 3]
+
+# pp uses the .pretty_inspect method
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+print "Pass this test, please."
